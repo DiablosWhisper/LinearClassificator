8f96fd136410b820050e4f3a239cee3911b003b4
DiablosWhisper <43128582+DiablosWhisper@users.noreply.github.com>
2019-02-06 20:46:50 +0200

    Initial commit

---
+++ LinearClassificator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LinearClassificator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPoints = new System.Windows.Forms.Button();
+            this.Box = new System.Windows.Forms.PictureBox();
+            this.Train = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawPoints
+            // 
+            this.DrawPoints.Location = new System.Drawing.Point(619, 13);
+            this.DrawPoints.Name = "DrawPoints";
+            this.DrawPoints.Size = new System.Drawing.Size(75, 23);
+            this.DrawPoints.TabIndex = 0;
+            this.DrawPoints.Text = "DrawPoints";
+            this.DrawPoints.UseVisualStyleBackColor = true;
+            this.DrawPoints.Click += new System.EventHandler(this.DrawPoints_Click);
+            // 
+            // Box
+            // 
+            this.Box.Location = new System.Drawing.Point(13, 13);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(600, 600);
+            this.Box.TabIndex = 1;
+            this.Box.TabStop = false;
+            // 
+            // Train
+            // 
+            this.Train.Location = new System.Drawing.Point(619, 42);
+            this.Train.Name = "Train";
+            this.Train.Size = new System.Drawing.Size(75, 23);
+            this.Train.TabIndex = 2;
+            this.Train.Text = "Train";
+            this.Train.UseVisualStyleBackColor = true;
+            this.Train.Click += new System.EventHandler(this.Train_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 614);
+            this.Controls.Add(this.Train);
+            this.Controls.Add(this.Box);
+            this.Controls.Add(this.DrawPoints);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawPoints;
+        private System.Windows.Forms.PictureBox Box;
+        private System.Windows.Forms.Button Train;
+    }
+}
+
---
+++ LinearClassificator/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Threading;
+
+namespace LinearClassificator
+{
+    public partial class Form1 : Form
+    {
+        private void DrawPoints_Click(object sender, EventArgs e)
+        {
+            Graphics Line = Box.CreateGraphics();
+
+            foreach(Point Point in Points)
+            {
+                Point.Draw();
+            }
+
+            Line.DrawLine(new Pen(Color.Black), 0, 0, Box.Width, Box.Height);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LinearClassificator = new Perceptron(2);
+
+            Points = new Point[200];
+
+            for (int Index = 0; Index < Points.Length; Index++)
+            {
+                Points[Index] = new Point(Box);
+
+                Thread.Sleep(100);
+            }
+        }
+        private void Train_Click(object sender, EventArgs e)
+        {
+            foreach (Point Point in Points)
+            {
+                double[] Inputs = { Point.CoordinateX, Point.CoordinateY };
+
+                LinearClassificator.Train(Inputs, Point.Target);
+
+                if(LinearClassificator.MakeDesicion(Inputs).Equals(Point.Target))
+                {
+                    Point.Draw(Color.Green, Point.CoordinateX + 7, Point.CoordinateY + 7, 6, 3);
+                }
+                else
+                {
+                    Point.Draw(Color.Red, Point.CoordinateX + 7, Point.CoordinateY + 7, 6, 3);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+        private Perceptron LinearClassificator { get; set; }
+        private Point[] Points { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Perceptron
+    {
+        public void Train(double[] Inputs, int Target)
+        {
+            int Error = Target - MakeDesicion(Inputs);
+
+            for(int Index = 0; Index < Inputs.Length; Index++)
+            {
+                Weights[Index] += Inputs[Index] * Error * LearningRate;
+            }
+        }
+        public int MakeDesicion(double[] Inputs)
+        {
+            double GlobalSum = 0;
+
+            for (int Index = 0; Index < Inputs.Length; Index++)
+            {
+                GlobalSum += Inputs[Index] * Weights[Index];
+            }
+
+            return Math.Sign(GlobalSum);
+        }
+        public double[] Weights { get; private set; }
+        public Perceptron(int NumberOfInputs)
+        {
+            LearningRate = 0.1;
+
+            Randomizer = new Random();
+
+            Weights = new double[NumberOfInputs];
+
+            for (int Index = 0; Index < Weights.Length; Index++)
+            {
+                Weights[Index] = Randomizer.NextDouble() - Randomizer.NextDouble();
+            }
+        }
+        private Random Randomizer { get; set; }
+        public double LearningRate { get; set; }
+    }
+
+    public class Point
+    {
+        public void Draw(Color Color, int CoordinateX, int CoordinateY, int Size, int Thickness)
+        {
+            Shape.DrawEllipse(new Pen(Color, Thickness), CoordinateX, CoordinateY, Size, Size);
+        }
+        public int CoordinateY { get; private set; }
+        public int CoordinateX { get; private set; }
+        private Random Randomizer { get; set; }
+        public int Target { get; private set; }
+        private Graphics Shape { get; set; }
+        private PictureBox Box { get; set; }
+        public Point(PictureBox Box)
+        {
+            Randomizer = new Random();
+
+            CoordinateX = Randomizer.Next(0, Box.Width);
+
+            CoordinateY = Randomizer.Next(0, Box.Height);
+
+            this.Box = Box;
+        }
+        public void Draw()
+        {
+            Shape = Box.CreateGraphics();
+
+            Shape.SmoothingMode = SmoothingMode.HighQuality;
+
+            if (CoordinateX > CoordinateY)
+            {
+                Shape.DrawEllipse(new Pen(Color.Blue), CoordinateX, CoordinateY, 20, 20);
+
+                Target = 1;
+            }
+            else
+            {
+                Shape.DrawEllipse(new Pen(Color.Red), CoordinateX, CoordinateY, 20, 20);
+
+                Target = -1;
+            }
+        }
+    }
+}
